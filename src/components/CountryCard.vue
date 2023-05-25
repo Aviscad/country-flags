8b7fd0924b95d58e57f757560864b267ce9a44f2
Aviscad <anthonyviscad@gmail.com>
2023-05-25 11:05:31 -0600

    Showing Details of Clicked Country

--- src/components/CountryCard.vue
+++ src/components/CountryCard.vue
@@ -22,7 +22,7 @@ const clicked = () => {
       :src="props.info.flags.svg && props.info.flags.png"
       :alt="props.info.flags.alt && `Image of the flag of ${props.info.name.common}`"
     />
-    <figcaption class="flex flex-col p-5 border-t-slate-200 border-t-[1.5px] h-44">
+    <figcaption class="flex flex-col p-5 border-t-slate-200 border-t-[1.5px] h-48 leading-loose">
       <h2 class="text-lg font-bold py-1">{{ props.info.name.common }}</h2>
       <div>
         <p><span class="font-semibold">Population:</span> {{ formatNum(props.info.population) }}</p>
--- src/components/InformationCard.vue
+++ src/components/InformationCard.vue
@@ -13,6 +13,32 @@ const { country } = useGetCountry(route.params.name, info)
 const { getCapitalName } = useGetCapital()
 const { formatNum } = useFormatNum()
 
+const getTopLvlDomains = (domains) => {
+  let helper = []
+  domains.forEach((domain) => {
+    helper.push(domain)
+  })
+  return helper.join(', ').trim()
+}
+
+const getCurrencies = (currencies) => {
+  let helper = []
+  Object.values(currencies).forEach((currency) => {
+    helper.push(`${currency.name} (${currency.symbol})`)
+  })
+  return helper.join(', ').trim()
+}
+
+const getLanguages = (languages) => {
+  let helper = []
+  Object.values(languages).forEach((language) => {
+    helper.push(language)
+  })
+  return helper.join(', ').trim()
+}
+
+window.scrollTo(0, 0)
+
 const close = () => {
   router.push({ name: 'home' })
 }
@@ -20,7 +46,7 @@ const close = () => {
 <template>
   <div class="relative min-h-screen w-full bg-slate-200 z-10 p-5 outline-none pt-20">
     <button @click="close" class="border border-gray-500 rounded-md px-2">&#8592; Back</button>
-    <div class="flex flex-col items-center justify-center p-5">
+    <div class="flex flex-col gap-5 p-5 leading-loose">
       <img
         class="object-cover"
         :src="country.flags.svg && country.flags.png"
@@ -29,21 +55,25 @@ const close = () => {
       <h3 class="font-semibold text-lg">{{ country.name.common }}</h3>
 
       <div>
+        <p>....</p>
         <p>
           <span class="font-semibold">Population:</span>
           {{ formatNum(country.population) }}
         </p>
         <p><span class="font-semibold">Region:</span> {{ country.region }}</p>
-        <p>
-          <span class="font-semibold">Capital:</span> {{ getCapitalName(country.value.capital) }}
-        </p>
-        <p></p>
+        <p><span class="font-semibold">Sub-Region:</span> {{ country.subregion }}</p>
+        <p><span class="font-semibold">Capital:</span> {{ getCapitalName(country.capital) }}</p>
       </div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit obcaecati repudiandae
-        pariatur ad, voluptatibus dolorum beatae quia, nisi repellendus optio dolor eius ipsum odit
-        sapiente est nesciunt. Quo, quaerat aut.
-      </p>
+      <div>
+        <p>
+          <span class="font-semibold">Top Level Domain:</span>
+          {{ getTopLvlDomains(country.tld) }}
+        </p>
+        <p>
+          <span class="font-semibold">Currencies:</span> {{ getCurrencies(country.currencies) }}
+        </p>
+        <p><span class="font-semibold">Languagues:</span> {{ getLanguages(country.languages) }}</p>
+      </div>
     </div>
   </div>
 </template>
