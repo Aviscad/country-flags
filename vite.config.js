7a6be7918f3792bc0608e29356784d9492b0e078
Manuel Viscarra <anthonyviscad@gmail.com>
2025-04-18 15:15:45 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/manuelviscarra.com/',
+  base: '/manuelviscarra.com/country-flags/',
   build: {
     emptyOutDir: true,
   },
